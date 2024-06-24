f37bc7fbc55c8a1e4908a26db479916c3d4ea4cb
big00002 <6629011018@cdti.ac.th>
2024-06-24 16:12:13 +0700

    update

---
+++ lab4.2.py
@@ -0,0 +1,7 @@
+#Tuple Type Mutable
+person = [35, 52 , 18.23, 195, 155]
+#for p in person:
+#    print(p)
+print(person[-4:-1])
+#person[0] = 100
+#print(person[0:])
---
+++ lab4.3.py
@@ -0,0 +1,9 @@
+person = {"name": "Thanwarat","age":17, "wt":55, "h":156}
+#for n in person:
+#    print(n)
+print(person["name"])
+print(person["age"])
+print(person["wt"])
+print(person["h"])
+person["name"] = "gai"
+print(person["name"])
